5737d3d3d11a4aadaab54bc257ad09bf3beaa170
borboncio <victor.borba.9@gmail.com>
2024-06-12 21:46:59 -0300

    Função removida pois nao foi ultilizada no projeto final

--- gerenciador.py
+++ gerenciador.py
@@ -1,7 +1,6 @@
 from class_listaduplamenteencadeada import DoublyCircularLinkedList
 from Class_tree import BinaryTree
 
-
 class Tarefa:
     def __init__(self, nome, descricao, prioridade):
         self.nome = nome
@@ -32,27 +31,6 @@ class GerenciadorDeTarefas:
             if node.key.prioridade:
                 self.tarefas_prioritarias.remove(node.key)
             print(f"Tarefa '{nome}' removida.")
-        else:
-            print(f"Tarefa '{nome}' não encontrada.")
-
-    def editar_tarefa(self, nome, novo_nome=None, nova_descricao=None, nova_prioridade=None):
-        node = self.tree.search(nome)
-        if node:
-            tarefa = node.key
-            if novo_nome:
-                self.tree.delete(nome)
-                tarefa.nome = novo_nome
-                self.tree.insert(tarefa)
-            if nova_descricao:
-                tarefa.descricao = nova_descricao
-            if nova_prioridade is not None:
-                if tarefa.prioridade != nova_prioridade:
-                    if tarefa.prioridade:
-                        self.tarefas_prioritarias.remove(tarefa)
-                    tarefa.prioridade = nova_prioridade
-                    if nova_prioridade:
-                        self.tarefas_prioritarias.append(tarefa)
-            print(f"Tarefa '{nome}' editada.")
         else:
             print(f"Tarefa '{nome}' não encontrada.")
 
